3c558dd14d1786f284eb2ad88c6f1fd716b0d7aa
zoheir kabuli <zoheyrk@gmail.com>
2022-12-02 00:09:16 +0330

    change next config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
