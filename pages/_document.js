e7c8ac7c00c51f425d2fdf6784ae61828933e11f
zoheir kabuli <zoheyrk@gmail.com>
2022-12-03 11:44:16 +0330

    add dynamic path to favicons

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,17 @@
 import Document, { Html, Main, NextScript, Head } from "next/document";
+import { useRouter } from "next/router";
 
 class MyDocument extends Document {
+  hostName = "";
+  static async getInitialProps(ctx) {
+    const { req, query, res, asPath, pathname } = ctx;
+    this.hostName = req.headers.host;
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
   render() {
     return (
       <Html dir="rtl" lang="fa">
@@ -8,21 +19,24 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href={`${this.hostName}/apple-touch-icon.png`}
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href={`${this.hostName}//favicon-32x32.png`}
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href={`${this.hostName}//favicon-16x16.png`}
           />
-          <link rel="manifest" href="/site.webmanifest"></link>
+          <link
+            rel="manifest"
+            href={`${this.hostName}/site.webmanifest`}
+          ></link>
         </Head>
         <body>
           <Main />
