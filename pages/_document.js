b1bd5aed2502d0a025f36507cfd9dfd5d684f865
zoheir kabuli <zoheyrk@gmail.com>
2022-12-03 11:47:56 +0330

    change favicons to static path

--- pages/_document.js
+++ pages/_document.js
@@ -2,16 +2,6 @@ import Document, { Html, Main, NextScript, Head } from "next/document";
 import { useRouter } from "next/router";
 
 class MyDocument extends Document {
-  hostName = "";
-  static async getInitialProps(ctx) {
-    const { req, query, res, asPath, pathname } = ctx;
-    this.hostName = req.headers.host;
-
-    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
-  }
   render() {
     return (
       <Html dir="rtl" lang="fa">
@@ -19,24 +9,21 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href={`${this.hostName}/apple-touch-icon.png`}
+            href="/blog-app/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href={`${this.hostName}//favicon-32x32.png`}
+            href="/blog-app/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href={`${this.hostName}//favicon-16x16.png`}
+            href="/blog-app/favicon-16x16.png"
           />
-          <link
-            rel="manifest"
-            href={`${this.hostName}/site.webmanifest`}
-          ></link>
+          <link rel="manifest" href="/blog-app/site.webmanifest"></link>
         </Head>
         <body>
           <Main />
